b68c3b471a2eba2e3b151adf7c8ecd61f3b2f275
Lazlo Mita <lazlo.mita@gmail.com>
2015-05-15 11:50:29 -0400

    Another change

--- exercises-resp/program3.js
+++ exercises-resp/program3.js
@@ -1,9 +1,27 @@
 var fs = require('fs')
 
+console.log(fs.readFileSync(process.argv[2], 'utf8').split('\n').length - 1)
+
+/*
+var fs = require('fs')
+
 var buf = fs.readFileSync(process.argv[2])
-
 var str = buf.toString()
+var res = str.split("\n").length - 1
+console.log(res)
+*/
 
-var res = str.split("\n").length
+/*
+var fs = require('fs')
 
-console.log(res);
+var contents = fs.readFileSync(process.argv[2])
+var lines = contents.toString().split('\n').length - 1
+console.log(lines)
+*/
+
+/*
+// note you can avoid the .toString() by passing 'utf8' as the
+// second argument to readFileSync, then you'll get a String!
+
+fs.readFileSync(process.argv[2], 'utf8').split('\n').length - 1
+*/
