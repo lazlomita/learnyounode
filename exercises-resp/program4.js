b04fcfecc5fcbd47d0f03209a8aaf350c2a88ce2
Lazlo Mita <lazlo.mita@gmail.com>
2015-05-19 10:06:03 -0400

    Adding Progrmam4

--- exercises-resp/program4.js
+++ exercises-resp/program4.js
@@ -15,3 +15,15 @@ fs.readFile(process.argv[2], function read(err, data) {
 function processFile() {
     console.log(content.toString().split('\n').length - 1);
 }
+
+/*
+var fs = require('fs')
+var file = process.argv[2]
+
+fs.readFile(file, function (err, contents) {
+  // fs.readFile(file, 'utf8', callback) can also be used
+  var lines = contents.toString().split('\n').length - 1
+  console.log(lines)
+})
+
+*/
